1a2457d61c150fbb28f350bdfaccd8cf10abd38b
JohanFarfan25 <johanfarfan25@gmail.com>
2021-03-29 22:04:54 -0500

    Actualización vista

--- src/componentes/Dropdown.js
+++ src/componentes/Dropdown.js
@@ -1,4 +1,21 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 
 const Dropdown = props => {
@@ -8,13 +25,32 @@ const Dropdown = props => {
 
     }
 
+    const classes = useStyles();
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
     return (
-        <div className="col-sm-12 form-group row">
-            <label className="form-label col-sm-4">{props.label}</label>
-            <select value={props.selectedValue} onChange={dropdownChanged} className="form-control form-control-sm col-sm-12">
-                <option key={0}>Select...</option>
-                {props.options.map((item, idx) => <option key={idx + 1} value={item.id}>{item.name}</option>)}
-            </select>
+
+
+
+        <div className="col-lg-12 form-group row">
+
+            <FormControl required className={classes.formControl}>
+                <InputLabel id="demo-simple-select-required-label">{props.label}</InputLabel>
+                <Select value={props.selectedValue} onChange={dropdownChanged}
+                    labelId="demo-simple-select-required-label"
+                    id="demo-simple-select-required">
+                    
+                    <MenuItem key={0}>
+                        <em>Select...</em>
+                    </MenuItem>
+                    {props.options.map((item, idx) => <MenuItem key={idx + 1} value={item.id}>{item.name}</MenuItem>)}
+                </Select>
+                <FormHelperText>Seleccionar...</FormHelperText>
+            </FormControl>
         </div>
 
     );
---
+++ src/componentes/content.js
@@ -0,0 +1,166 @@
+import React, { useState, useEffect } from 'react';
+import Dropdown from './Dropdown';
+import Listbox from './Listbox';
+import Detail from './Detail';
+import { Credentials } from './Credentials';
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
+
+
+const Contenido = () => {
+    const spotify = Credentials();
+    console.log('RENDERING APP.JS');
+
+    const data = [
+
+        { Value: 1, name: 'A' },
+        { Value: 2, name: 'B' },
+        { Value: 3, name: 'C' }
+
+    ];
+
+    const [token, setToken] = useState('');
+    const [genres, setGenres] = useState({ selectedGenre: '', listOfGenresFromAPI: [] });
+    const [playlist, setPlaylist] = useState({ selectedPlaylist: '', listOfPlaylistFromAPI: [] });
+    const [tracks, setTracks] = useState({ SelectedTracks: '', listOfTracksFromAPI: [] });
+    const [trackDetail, setTrackDetail] = useState(null);
+
+    useEffect(() => {
+
+
+        axios('https://accounts.spotify.com/api/token', {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)
+            },
+            data: 'grant_type=client_credentials',
+            method: 'POST'
+        })
+            .then(tokenResponse => {
+                setToken(tokenResponse.data.access_token);
+
+                axios('https://api.spotify.com/v1/browse/categories?locale=sv_US', {
+                    method: 'GET',
+                    headers: { 'Authorization': 'Bearer ' + tokenResponse.data.access_token }
+                })
+                    .then(genreResponse => {
+                        setGenres({
+                            selectedGenre: genres.selectedGenre,
+                            listOfGenresFromAPI: genreResponse.data.categories.items
+                        })
+                    });
+
+            });
+
+
+    }, [genres.selectedGenre, spotify.ClientId, spotify.ClientSecret]);
+
+    const genreChanged = val => {
+        setGenres({
+            selectedGenre: val,
+            listOfGenresFromAPI: genres.listOfGenresFromAPI
+        });
+
+        axios(`https://api.spotify.com/v1/browse/categories/${val}/playlists?limit=10`, {
+            method: 'GET',
+            headers: { 'Authorization': 'Bearer ' + token }
+        })
+            .then(playlistResponse => {
+                setPlaylist({
+                    selectedPlaylist: playlist.selectedPlaylist,
+                    listOfPlaylistFromAPI: playlistResponse.data.playlists.items
+                })
+            });
+
+        console.log(val);
+    }
+
+
+    const playlistChanged = val => {
+        console.log(val);
+        setPlaylist({
+            selectedPlaylist: val,
+            listOfPlaylistFromAPI: playlist.listOfPlaylistFromAPI
+        });
+    }
+
+    const buttonClicked = e => {
+        e.preventDefault();
+
+        axios(`https://api.spotify.com/v1/playlists/${playlist.selectedPlaylist}/tracks?limit=10`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+
+            .then(tracksResponse => {
+                setTracks({
+
+                    SelectedTracks: tracks.SelectedTracks,
+                    listOfTracksFromAPI: tracksResponse.data.items
+                })
+
+            });
+    }
+
+    const listboxClicked = val => {
+
+        const currentTracks = [...tracks.listOfTracksFromAPI];
+
+        const trackInfo = currentTracks.filter(t => t.track.id === val);
+
+        setTrackDetail(trackInfo[0].track);
+
+    }
+
+    return (
+
+
+        <div className="container col-md-12" >
+
+            <form onSubmit={buttonClicked} className="row">
+                <div className="col-sm-4">
+                    <Dropdown label="Genero :" options={genres.listOfGenresFromAPI} selectedValue={genres.selectedGenre} changed={genreChanged} />
+
+                    <Dropdown label="Lista  :" options={playlist.listOfPlaylistFromAPI} selectedValue={playlist.selectedPlaylist} changed={playlistChanged} />
+                    <div className="col-sm-12 form-group px-0">
+                        <Button type='submit' variant="contained" color="primary" disableElevation> Buscar </Button>
+                    </div>
+                </div>
+
+
+                <div className="Row col-lg-8" data-toggle="modal" data-target="#smallmodal">
+                    <h3>Lista de Canciones</h3>
+                    <Listbox label="" items={tracks.listOfTracksFromAPI} clicked={listboxClicked} />
+                </div>
+
+
+
+            </form>
+
+            <div className="modal fade" id="smallmodal" tabindex="-1" role="dialog" aria-labelledby="smallmodalLabel"
+                aria-hidden="true">
+
+                <div className="modal-dialog modal-sm" role="document" >
+                    <div className="modal-content" >
+                        <div className="modal-header" ></div>
+                        <div className="modal-body" > {
+                            trackDetail && < Detail {...trackDetail}
+                            />} </div> <div className="modal-footer" >
+                            <Button variant="contained" color="primary" disableElevation data-dismiss="modal" >
+                                Regresar
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+    );
+
+}
+
+export default Contenido;
